d517c2db1782f750367b8fde42a120b4dc9cbc68
Ty Kennington <tyjkenn@gmail.com>
2018-03-22 23:30:46 -0600

    convert from latitude/longitude to pixels

--- dem_reader.py
+++ dem_reader.py
@@ -2,24 +2,49 @@ from PIL import Image
 import numpy as np
 
 
+def convert_coordinates(coordinates):
+    for i in range(len(coordinates)):
+        coordinates[i] = (int((50 - coordinates[i][0]) * 480), int((coordinates[i][1] + 90) * 480))
+
+
+def get_new_size(size):
+    return int(size[0] * 480), int(size[1] * 480)
+
+
 def get_samples(filename, coordinates, size):
+    convert_coordinates(coordinates)
+    size = get_new_size(size)
     Image.MAX_IMAGE_PIXELS = 1000000000
     im = Image.open(filename)
     full_map = np.array(im)
     samples = []
     for co in coordinates:
-        sample = full_map[co[0]-size[0]:co[0]+size[0],co[1]-size[1]:co[1]+size[1]]
+        sample = full_map[co[0]-size[0]:co[0]+size[0], co[1]-size[1]:co[1]+size[1]]
         samples.append(sample)
+        img = Image.fromarray(sample, 'I')
+        img = img.point(lambda p: p * 0.05)
+        img.show()
     return samples
 
+
+def calc_slopes(sample):
+    slopes = []
+    for x in range(sample.shape[0] - 1):
+        for y in range(sample.shape[1] - 1):
+            slopes.append(sample[x, y] - sample[x - 1, y - 1])
+            slopes.append(sample[x, y] - sample[x + 1, y - 1])
+            slopes.append(sample[x, y] - sample[x - 1, y + 1])
+            slopes.append(sample[x, y] - sample[x + 1, y + 1])
+    return slopes
 
 def analyze_sample(sample):
     mini = sample.min()
     maxi = sample.max()
     avg = sample.mean()
     stdev = sample.std()
-    # TODO: calc slopes
-    return [mini, maxi, avg, stdev]
+    slopes = calc_slopes(sample)
+    slope_std = np.std(slopes)
+    return [mini, maxi, avg, stdev, slope_std]
 
 
 def analyze_samples(samples):
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import dem_reader as dr
 
-samples = dr.get_samples('30n120w_20101117_gmted_max075.tif', [(300, 300)], (100, 100))
+sample_coords = [
+    (36.0544, -112.1401)  # grand canyon
+]
+
+samples = dr.get_samples('30n120w_20101117_gmted_max075.tif', sample_coords, (.5, .5))
 analysis = dr.analyze_samples(samples)
 print(analysis)
