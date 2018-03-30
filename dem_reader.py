96b48b02c9af50969860891c9b91dbdcc2707f34
Ty Kennington <tyjkenn@gmail.com>
2018-03-30 15:54:39 -0600

    analyze entire map based of classification and clustering

--- dem_reader.py
+++ dem_reader.py
@@ -2,11 +2,15 @@ from PIL import Image
 import numpy as np
 import plotly
 import plotly.graph_objs as go
+import random as rd
 
 
 def convert_coordinates(coordinates):
     for i in range(len(coordinates)):
-        coordinates[i] = (int((50 - coordinates[i][0]) * 480), int((coordinates[i][1] + 90) * 480))
+        if isinstance(coordinates[i], list):
+            convert_coordinates(coordinates[i])
+        else:
+            coordinates[i] = (int((50 - coordinates[i][0]) * 480), int((coordinates[i][1] + 90) * 480))
 
 
 def get_new_size(size):
@@ -41,16 +45,22 @@ def display_sample_3d(sample):
     plotly.offline.plot(fig)
 
 
-def get_samples(filename, coordinates, size):
-    convert_coordinates(coordinates)
-    size = get_new_size(size)
-    Image.MAX_IMAGE_PIXELS = 1000000000
-    im = Image.open(filename)
-    full_map = np.array(im)
+def get_samples(dem, coordinates, size):
+    if isinstance(dem, str):
+        convert_coordinates(coordinates)
+        size = get_new_size(size)
+        Image.MAX_IMAGE_PIXELS = 1000000000
+        im = Image.open(dem)
+        full_map = np.array(im)
+    else:
+        full_map = dem
     samples = []
     for co in coordinates:
-        sample = full_map[co[0]-size[0]:co[0]+size[0], co[1]-size[1]:co[1]+size[1]]
-        samples.append(sample)
+        if isinstance(co, list):
+            samples.append(get_samples(full_map, co, size))
+        else:
+            sample = full_map[co[0]-size[0]:co[0]+size[0], co[1]-size[1]:co[1]+size[1]]
+            samples.append(sample)
     # test sample
     # display_sample_3d(sample)
     return samples
@@ -81,11 +91,34 @@ def analyze_sample(sample):
     for slope in slopes:
         if slope >= 50:
             steep_count += 1
-    return [mini, maxi, avg, stdev, slope_std, steep_count]
+    return [mini, maxi, avg, stdev, slope_std, steep_count / 100]
 
 
 def analyze_samples(samples):
     result = []
     for sample in samples:
-        result.append(analyze_sample(sample))
+        if isinstance(sample, list):
+            result.append(analyze_samples(sample))
+        else:
+            result.append(analyze_sample(sample))
     return result
+
+
+def pick_colors(classes):
+    colors = []
+    for i in range(len(classes)):
+        color = (rd.randrange(0, 256), rd.randrange(0, 256), rd.randrange(0, 256))
+        colors.append(color)
+    return colors
+
+
+def draw_class_map(filename, sample_map, model, classes):
+    colors = pick_colors(classes)
+    im = Image.new("RGB", (len(sample_map[0]), len(sample_map)), "white")
+    for i in range(len(sample_map)):
+        for j in range(len(sample_map[i])):
+            pred = model.predict([sample_map[i][j]])[0]
+            color = colors[classes.index(pred)]
+            im.putpixel((j, len(sample_map) - i - 1), color)
+    im.save(filename, "PNG")
+    return im
--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from sklearn.naive_bayes import GaussianNB
 import random as rd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import numpy as np
+from sklearn.cluster import KMeans
 
+
+# increases the size of the coordinate list by varying
+# the given coordinates uniformly no further than dev
+# latitude or longitude away
 def vary_coordinates(coords, classes, times, dev):
     new_coords = []
     new_class = []
@@ -14,6 +20,43 @@ def vary_coordinates(coords, classes, times, dev):
     return new_coords, new_class
 
 
+def get_coord_grid(minLat, minLon, maxLat, maxLon, size):
+    coords = []
+    i = minLat + size
+    while i < maxLat - size:
+        coord_row = []
+        j = minLon + size
+        while j < maxLon - size:
+            coord_row.append((i, j))
+            j += size * 2
+        coords.append(coord_row)
+        i += size * 2
+    return coords
+
+
+def map_classifications(model, sample_width):
+    coords = get_coord_grid(30, -120, 50, -90, .1)
+    samples = dr.get_samples('30n120w_20101117_gmted_max075.tif', coords, (sample_width, sample_width))
+    data = dr.analyze_samples(samples)
+    dr.draw_class_map("classification_map.png", data, model, ['mountain', 'plains', 'canyon', 'random'])
+
+
+def make_cluster_map(cluster_count, sample_width):
+    coords = get_coord_grid(30, -120, 50, -90, sample_width)
+    samples = dr.get_samples('30n120w_20101117_gmted_max075.tif', coords, (sample_width, sample_width))
+    data = dr.analyze_samples(samples)
+    model = KMeans(n_clusters=cluster_count).fit([item for sublist in data for item in sublist])
+    dr.draw_class_map("cluster_map.png", data, model, range(cluster_count))
+
+
+def add_random_samples(samples, classes, count):
+    rdmaps = np.asarray([[[rd.randrange(0, 2000) for i in range(120)] for j in range(120)] for k in range(count)])
+    samples.extend(rdmaps)
+    classes.extend(['random'] * count)
+
+
+# tuples of latitude followed by longitude. Note that
+# north and east are the positive directions
 coordinates = [
     (36.0544, -112.1401),  # grand canyon
     (44.4280, -110.5885),  # yellowstone
@@ -41,13 +84,37 @@ classes = [
 
 full_coord, full_class = vary_coordinates(coordinates, classes, 10, .4)
 
-samples = dr.get_samples('30n120w_20101117_gmted_max075.tif', full_coord, (.25, .25))
+# convert the coordinates into samples, then take summaries of the samples.
+# Then split the summaries for training and testing
+samples = dr.get_samples('30n120w_20101117_gmted_max075.tif', full_coord, (.1, .1))
+add_random_samples(samples, full_class, 10)
 data = dr.analyze_samples(samples)
-
 train_data, test_data, train_target, test_target = train_test_split(data, full_class, test_size=.3, train_size=.7, shuffle=True)
 
+# Time to learn!
 gnb = GaussianNB()
-pred = gnb.fit(train_data, train_target).predict(test_data)
+model = gnb.fit(train_data, train_target)
+pred = model.predict(test_data)
 score = accuracy_score(test_target, pred)
 
+# Time to score!
 print("Score: " + ("%.1f" % (score * 100)) + "%")
+
+
+# Try some random and flat samples
+rdmaps = np.asarray([[[rd.randrange(0, 2000) for i in range(120)] for j in range(120)] for k in range(10)])
+flatmaps = np.asarray([[[50 for i in range(120)] for j in range(120)] for k in range(10)])
+rddata = dr.analyze_samples(rdmaps)
+flatdata = dr.analyze_samples(flatmaps)
+rdpred = model.predict(rddata)
+flatpred = model.predict(flatdata)
+print('Classification of random terrains:')
+print(rdpred)
+print('Classification of flat samples')
+print(flatpred)
+
+# Map based on training
+map_classifications(model, .1)
+
+# Try clustering
+make_cluster_map(7, 1)
